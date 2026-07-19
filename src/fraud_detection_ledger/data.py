ad3ee47878228512daf5d1256c607bbc144c8f5a
HyunSu <mosjeong1@gmail.com>
2026-07-19 18:28:16 +0900

    Update data.py

--- src/fraud_detection_ledger/data.py
+++ src/fraud_detection_ledger/data.py
@@ -6,7 +6,7 @@ def load_data(filename: str) -> pd.DataFrame:
     상대 경로를 기반으로 CSV 파일을 불러옵니다.
     날짜 및 시간 컬럼도 datetime 형식으로 파싱합니다.
     """
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    base_dir = os.path.abspath(os.getcwd())
     full_path = os.path.join(base_dir, filename)
 
     df = pd.read_csv(full_path)
@@ -22,7 +22,7 @@ def save_data(df: pd.DataFrame, filename: str):
     """
     상대 경로를 기반으로 CSV 파일로 저장합니다.
     """
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    base_dir = os.path.abspath(os.getcwd())
     full_path = os.path.join(base_dir, filename)
     df.to_csv(full_path, index=False)
 
